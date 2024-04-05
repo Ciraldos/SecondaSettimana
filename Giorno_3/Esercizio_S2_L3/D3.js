68816cd4f118a2e8cc9dafe51adba401e4f42e7b
nomeutentegithub <andreaciraldo2@gmail.com>
2024-04-05 12:59:46 +0200

    new file:   Giorno_4/date.html 	new file:   Giorno_4/funzioni-scope.html 	new file:   Giorno_4/funzioni.html 	new file:   Giorno_4/math.html 	new file:   Giorno_4/orologio.html 	new file:   Giorno_4/stringhe.html

--- Giorno_3/Esercizio_S2_L3/D3.js
+++ Giorno_3/Esercizio_S2_L3/D3.js
@@ -211,6 +211,9 @@ switch(true){
   Usa un for loop per cambiare il valore della proprietà "gender" di alcuni personaggi dal valore "n/a" a "robot" (Tip: puoi effettuare la riassegnazione del valore corrispondente o creare un nuovo array)
 */
 
+
+
+
 for(i=0; i<starWarsCharacters.length; i++){
   if(starWarsCharacters[i].gender ==="n/a"){
     starWarsCharacters[i].gender = "robot";
