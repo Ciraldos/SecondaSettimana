87f05fc5ddaa92632c23cf02fdc536d3cdf0c895
nomeutentegithub <andreaciraldo2@gmail.com>
2024-04-07 20:04:41 +0200

    modified:   Giorno_4/Progetto_Seconda_Settimana/JS Exercises/D5.js

--- Giorno_4/Progetto_Seconda_Settimana/JS Exercises/D5.js
+++ Giorno_4/Progetto_Seconda_Settimana/JS Exercises/D5.js
@@ -136,63 +136,90 @@ while (i < numericArray.length && numericArray[i] !== 32) {
 */
 const charactersArray = ['g', 'n', 'u', 'z', 'd']
 
-const positionsArray = charactersArray.map(char => {
-  switch (char) {
-    case 'a':
-      return 1;
-    case 'b':
-      return 2;
-    case 'c':
-      return 3;
-    case 'd':
-      return 4;
-    case 'e':
-      return 5;
-    case 'f':
-      return 6;
-    case 'g':
-      return 7;
-    case 'h':
-      return 8;
-    case 'i':
-      return 9;
-    case 'j':
-      return 10;
-    case 'k':
-      return 11;
-    case 'l':
-      return 12;
-    case 'm':
-      return 13;
-    case 'n':
-      return 14;
-    case 'o':
-      return 15;
-    case 'p':
-      return 16;
-    case 'q':
-      return 17;
-    case 'r':
-      return 18;
-    case 's':
-      return 19;
-    case 't':
-      return 20;
-    case 'u':
-      return 21;
-    case 'v':
-      return 22;
-    case 'w':
-      return 23;
-    case 'x':
-      return 24;
-    case 'y':
-      return 25;
-    case 'z':
-      return 26;
-    default:
-      return null; 
-  }
-});
+let positionsArray=[];
+for (let i=0;i<charactersArray.length;i++){
+    switch (charactersArray[i]) {
+      case 'a':
+        positionsArray[i]=1;
+        break;
+      case 'b':
+        positionsArray[i]=2;
+        break;
+      case 'c':
+        positionsArray[i]=3;
+        break;
+      case 'd':
+        positionsArray[i]=4;
+        break;
+      case 'e':
+        positionsArray[i]=5;
+        break;
+      case 'f':
+        positionsArray[i]=6;
+        break;
+      case 'g':
+        positionsArray[i]=7;
+        break;
+      case 'h':
+        positionsArray[i]=8;
+        break;
+      case 'i':
+        positionsArray[i]=9;
+        break;
+      case 'j':
+        positionsArray[i]=10;
+        break;
+      case 'k':
+        positionsArray[i]=11;
+        break;
+      case 'l':
+        positionsArray[i]=12;
+        break;
+      case 'm':
+        positionsArray[i]=13;
+        break;
+      case 'n':
+        positionsArray[i]=14;
+        break;
+      case 'o':
+        positionsArray[i]=15;
+        break;
+      case 'p':
+        positionsArray[i]=16;
+        break;
+      case 'q':
+        positionsArray[i]=17;
+        break;
+      case 'r':
+        positionsArray[i]=18;
+        break;
+      case 's':
+        positionsArray[i]=19;
+        break;
+      case 't':
+        positionsArray[i]=20;
+        break;
+      case 'u':
+        positionsArray[i]=21;
+        break;
+      case 'v':
+        positionsArray[i]=22;
+        break;
+      case 'w':
+        positionsArray[i]=23;
+        break;
+      case 'x':
+        positionsArray[i]=24;
+        break;
+      case 'y':
+        positionsArray[i]=25;
+        break;
+      case 'z':
+        positionsArray[i]=26;
+        break;
+      
+    }
+}
+  
 
 console.log(positionsArray); 
